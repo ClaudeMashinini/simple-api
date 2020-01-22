e4dfbd7257f762afa9f2a05482184d9ce3988031
Claude Mashinini <dumi.claude@gmail.com>
2020-01-22 13:46:07 +0200

    index.js file

--- index.js
+++ index.js
@@ -1,65 +1,146 @@
-const { Client } = require("pg");
-const client = new Client({
-  user: "user",
-  password: "pass",
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("db", "user", "pass", {
   host: "localhost",
-  port: 5433,
-  database: "db"
+  dialect: "postgres",
+  pool: {
+    max: 9,
+    min: 0,
+    idle: 10000
+  }
 });
 
-CREATE TABLE visitors(
-  id serial PRIMARY KEY,
-  visitor_name VARCHAR(50) NOT NULL,
-  visitor_age integer NOT NULL,
-  date_of_visit VARCHAR (50) NOT NULL,
-  time_of_visit VARCHAR(50) NOT NULL,
-  assistant_name VARCHAR (50) not null,
-  visit_comments VARCHAR (100) not null
-)
+sequelize.authenticate().then(() => {
+  console.log("Success!");
+  const Visitors = sequelize.define(
+    "visitors",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        unique: true,
+        allowNull: false,
+        autoIncrement: true
+      },
+      visitor_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      visitor_age: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      date_of_visit: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      time_of_visit: {
+        type: Sequelize.TIME,
+        allowNull: false
+      },
+      assistant: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      visit_comments: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    },
+    {
+      freezelogName: true
+    }
+  );
 
-async function showAll() {
-  try {
-    await client.connect();
-    const { rows } = await client.query("select * from visitors");
-    console.table(rows);
-  } catch (error) {
-    console.error();
-  }
-}
+  const addNewVisitor = async (
+    visitor_name,
+    visitor_age,
+    date_of_visit,
+    time_of_visit,
+    assistant,
+    visit_comments
+  ) => {
+    Visitors.sync({ alter: true }).then(function() {
+      return Visitors.create({
+        visitor_name: visitor_name,
+        visitor_age: visitor_age,
+        date_of_visit: date_of_visit,
+        time_of_visit: time_of_visit,
+        assistant: assistant,
+        visit_comments: visit_comments
+      });
+    });
+    console.error;
+  };
 
-async function viewVisitor(id) {
-  try {
-    await client.connect();
-    const { rows } = await client.query("select * from visitors where id=$1", [id]);
-    console.table(rows)
-  } catch (error) {
-    console.error();
-  }
-}
+  const viewAll = async () => {
+    let array = [];
+    await Visitors.findAll({}).then(data => {
+      console.log(data);
+    });
+    console.error;
+  };
 
-async function deleteAll() {
-  try {
-    await client.connect();
-    await client.query("DELETE * from visitors");
-  } catch (error) {
-    console.error();
-  }
-}
-async function deleteVisitor(id) {
-  try {
-    await client.connect();
-    const { rows } = await client.query("DELETE from visitors where id=$1", [id]);
-    console.table(rows)
-  } catch (error) {
-    console.error();
-  }
-}
+  const viewVisitor = async id => {
+    await Visitors.findAll({
+      where: {
+        id: id
+      }
+    }).then(data => {
+      console.log(data);
+    });
+    console.error;
+  };
 
-async function addVisitor(visitor_name, visitor_age, date_of_visit, time_of_visit, assistant_name, visit_comments) {
-  try {
-    await client.connect();
-    await client.query("INSERT INTO visitors( visitor_name, visitor_age, date_of_visit, time_of_visit, assistant_name, visit_comments) VALUES($1,$2,$3,$4,$5,$6);", [visitor_name, visitor_age, date_of_visit, time_of_visit, assistant_name, visit_comments])
-    const { rows } = await client.query("select * from visitors");
-    console.table(rows);
-  }
-}
+  const deleteVisitor = async id => {
+    await Visitors.destroy({
+      where: {
+        id: id
+      }
+    }).then(data => {
+      console.log("Visitor removed from database");
+    });
+    console.error;
+  };
+  const updateVisitor = async (
+    id,
+    visitor_name,
+    visitor_age,
+    date_of_visit,
+    time_of_visit,
+    assistant,
+    visit_comments
+  ) => {
+    await Visitors.update(
+      {
+        visitor_name: visitor_name,
+        visitor_age: visitor_age,
+        date_of_visit: date_of_visit,
+        time_of_visit: time_of_visit,
+        assistant: assistant,
+        visit_comments: visit_comments
+      },
+      {
+        where: {
+          id: id
+        }
+      }
+    );
+    console.error;
+  };
+
+  const deleteAll = async () => {
+    await Visitors.destroy({
+      truncate: true
+    }).then(console.log("Database emptied"));
+    console.error;
+  };
+
+  module.exports = {
+    deleteAll: truncate,
+    deleteVisitor: remove,
+    updateVisitor: update,
+    addNewVisitor: add,
+    viewVisitor: view1,
+    viewAll: view
+  };
+});
